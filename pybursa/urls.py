4d2d0c9731d213e17d2745e2aeb04902ccb8f894
mishabeliy15 <misha.beliy15@gmail.com>
2017-05-13 16:33:10 +0300

    kz4_quadratic app

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^quadratic/', include('quadratic.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
