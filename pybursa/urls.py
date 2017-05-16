35e883f41ef6eec5a8ca15caf0f8d620eaf378c7
Valerii <ponomarenko.valerij@gmai.com>
2017-05-16 22:12:03 +0300

    add kr3.2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from pybursa.views import index, contact, student_list, student_detail
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^contact/$', contact, name='contact'),
+    url(r'^student_list/$', student_list, name='student_list'),
+    url(r'^student_detail/$', student_detail, name='student_detail'),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
