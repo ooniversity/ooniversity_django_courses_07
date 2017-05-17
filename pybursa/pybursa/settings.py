4369e9f780e42382410a5b63ba00c4f56f80c48d
israodin <israodin@gmail.com>
2017-05-17 13:48:36 +0300

    new models on  polls app
    Signed-off-by: Israodin <israodin@gmail.com>

--- pybursa/pybursa/settings.py
+++ pybursa/pybursa/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+    'polls.apps.PollsConfig',
+    # 'polls',
 ]
 
 MIDDLEWARE = [
