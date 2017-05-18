7d466d51213208c15bf28163f78f6ff7f8eaaa5b
Valerii <ponomarenko.valerij@gmai.com>
2017-05-18 07:43:04 +0300

    add format %2f

--- quadratic/views.py
+++ quadratic/views.py
@@ -26,17 +26,17 @@ def quadratic_results(request):
         dict_quadratic["resC"] = "коэффициент не целое число"
     if c == '':
         dict_quadratic["resC"] = "коэффициент не определён"
-    if type(a) == int and type(b) == int and type(c) == int:
+    if type(a) == int and type(b) == int and type(c) == int and a != 0:
         d = b ** 2 - 4 * a * c
         dict_quadratic["discriminant"] = "Дискриминант: %d" %(d)
         if d > 0:
             x1 = (-b + (d ** 0.5)) / (2 * a)
             x2 = (-b - (d ** 0.5)) / (2 * a)
             print(x1)
-            dict_quadratic["two_x"] = "Квадратное уравнение имеет два действительных корня: x1 = %d, x2 = %d" %(x1, x2)
+            dict_quadratic["two_x"] = "Квадратное уравнение имеет два действительных корня: x1 = %.1f, x2 = %.1f" %(x1, x2)
         elif d == 0:
             x1 = -b / (2 * a)
-            dict_quadratic["one_x"] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %d" %(x1)
+            dict_quadratic["one_x"] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %.1f" %(x1)
         elif d < 0:
             dict_quadratic["not_x"] = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
     return render(request, "results.html", dict_quadratic)
