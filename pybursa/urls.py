dd2b5748555d671c74637c4751c570f78a4a437d
Israodin <israodin@gmail.com>
2017-05-21 15:11:40 +0300

    qudratic
    Signed-off-by: Israodin <israodin@gmail.com>

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -25,6 +25,7 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/',include('polls.urls')),
+    url(r'^quadratic/',include('quadratic.urls')),
     url(r'^contact/$',views.contact ,name = 'contact'),
     url(r'^student_list/$',views.student_list,name = 'student_list'),
     url(r'^student_detail/$',views.student_detail,name ='student_detail'),
