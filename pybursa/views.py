8d34d4b8d9a42b11dc74b4a00dc1ef92f3bb4ec1
fon_harry <fon.harry@gmail.com>
2017-05-21 18:53:01 +0300

    Init quadratic app

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
+
 
 def contact(request):
-	return render(request, 'contact.html')
+    return render(request, 'contact.html')
+
 
 def student_list(request):
-	return render(request, 'student_list.html')
+    return render(request, 'student_list.html')
+
 
 def student_detail(request):
     return render(request, 'student_detail.html')
