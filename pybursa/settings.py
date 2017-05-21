72b905642290ba446604a1ff725f9f830255e7ce
Israodin <israodin@gmail.com>
2017-05-21 14:39:23 +0300

    change structore
    Signed-off-by: Israodin <israodin@gmail.com>

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quadratic',
-    'courses',
-    'students',
+
 ]
 
 MIDDLEWARE = [
