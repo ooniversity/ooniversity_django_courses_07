4fea9c8ce8bbf14901a0e36d5dcb31723e266b09
Stas_Bulkin <bulkin.stanislav@omega-auto.biz>
2017-05-22 21:12:04 +0300

    add polls template

--- pybursa/views.py
+++ pybursa/views.py
@@ -17,4 +17,4 @@ def student_list(request):
     return render(request, 'student_list.html')
 
 def student_detail(request):
-return render(request, 'student_detail.html')
+	return render(request, 'student_detail.html')
