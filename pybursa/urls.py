1a9c83a121501403533cbe363078769cdfd1a290
Eugene K <eugenek@nanorep.com>
2017-05-22 14:52:33 +0300

    Added templates to django

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
+
 urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	url(r'^contact/$', views.contact, name='contact'),
+	url(r'^student_list/$', views.student_list, name='student_list'),
+	url(r'^student_detail/$', views.student_detail, name='student_detail'),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
