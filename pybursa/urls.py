7795fb82486f801d8a41d3e120f3f21701bcee1c
mishabeliy15 <misha.beliy15@gmail.com>
2017-05-24 21:28:21 +0300

    Add contact.html. Add static

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from pybursa.views import contact
 
 urlpatterns = [
     url(r'^quadratic/', include('quadratic.urls')),
@@ -7,4 +8,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('courses.urls')),
     url(r'^',include('students.urls')),
+    url(r'^contact/',contact,name='contact')
 ]
