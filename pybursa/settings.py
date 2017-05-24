8f3bb338fb55c2fd09bb4e68aef0793eb5c3d7a1
ellinnyk <mel_hel2002@yahoo.com>
2017-05-24 13:05:10 +0300

    Added quadratic

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pybursa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'), os.path.join(BASE_DIR,'quadratic/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
