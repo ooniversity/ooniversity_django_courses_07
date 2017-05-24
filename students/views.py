49f4ddd0f13afcd232ba7ab2ca88256d89af1fd3
vKim <link@vkim@gmail.com>
2017-05-25 02:58:06 +0300

    KZ5v4

--- students/views.py
+++ students/views.py
@@ -5,6 +5,8 @@ from students.models import Student
 def list_view(request):
     if 'course_id' in request.GET and request.GET['course_id'] and int(request.GET['course_id']) > 0:
         context = {'students': Student.objects.filter(courses=request.GET['course_id'])}
+        if not context['students'].count():
+        	context = {'students': Student.objects.all()}
     else:
         context = {'students': Student.objects.all()}
     return render(request, 'students/list.html', context)
