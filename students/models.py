791cb8eac7a8c7cd16f4b32db1e486e3dbe6c1c7
Yuriy <garazdcreation@gmail.com>
2017-05-25 17:14:37 +0300

    Add apps Courses and Students

---
+++ students/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from courses.models import Course
+
+class Student(models.Model):
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+    date_of_birth = models.DateField(null=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=30)
+    address = models.CharField(max_length=254)
+    skype = models.CharField(max_length=64)
+    courses = models.ManyToManyField(Course)
+
+    #def __str__(self):
+    #    return self.subject
+
