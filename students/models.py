0d9404830a836d0157e5812b6a19adbb2e6380e7
Vasilev A <crawless@bk.ru>
2017-05-27 02:09:24 +0300

    Task #5_1

---
+++ students/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from courses.models import Course
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=64)
+    address = models.CharField(max_length=255)
+    skype = models.CharField(max_length=64)
+    courses = models.ManyToManyField(Course)
+
+    def __str__ (self):
+        return self.name
