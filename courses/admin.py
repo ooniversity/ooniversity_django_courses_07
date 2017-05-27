3929ebe9141edf50e9512c15ac998109b801c46f
Evgeniy <lepiloff82@gmail.com>
2017-05-27 22:23:42 +0300

    Add files via upload

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-admin.site.register(Course)
-admin.site.register(Lesson)
 # Register your models here.
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    fields = ["subject", 'description', 'order']
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ['name', 'short_description']
+
+    fieldsets = [
+        (None, {'fields': ["name", "short_description", "description"]}),
+    ]
+    inlines = [LessonInline]
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson)
