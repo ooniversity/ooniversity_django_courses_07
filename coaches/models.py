851b0d77e48c30b7e9043df8e42c227eab7beb1f
Yuriy <garazdcreation@gmail.com>
2017-05-27 09:13:21 +0300

    Migrate model coaches

--- coaches/models.py
+++ coaches/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Coach(models.Model):
     user = models.OneToOneField(User)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
     phone = models.CharField(max_length=30)
-    address = models.CharField()
-    skype = models.CharField()
+    address = models.CharField(max_length=255)
+    skype = models.CharField(max_length=64)
     description = models.TextField()
 
     def __str__(self):
