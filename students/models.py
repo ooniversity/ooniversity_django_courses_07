b4efe33ad056fb293f73a462cc1588c572478d1a
israodin <israodin@gmail.com>
2017-05-27 23:56:10 +0300

    courses detail

---
+++ students/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from courses.models import Course
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True,blank=True)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    skype = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course)
