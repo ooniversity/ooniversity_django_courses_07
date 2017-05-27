bf0f53859973f6430d0acb37b8c517e07102a928
tarchanskyj <ihp.arthur@gmail.com>
2017-05-27 17:38:56 +0300

    Before migrations

--- coaches/models.py
+++ coaches/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Coach(models.Model):
     user = models.OneToOneField(User)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=1, choises=('M', 'Male'), ('F', 'Female'))
+    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
     phone = models.CharField(max_length=15)
     address = models.CharField(max_length=255)
     skype = models.CharField(max_length=255)
--- coaches/views.py
+++ coaches/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from coaches.models import Coach
+
+
+def detail(request, coach_id):
+    
+    return render(request, 'courses/detail.html')
+
+
+
 
 # Create your views here.
