060c7c2d17344d72ba81f5e034a4b14988921b71
Egor Zhizhenko <hrabryi@gmail.com>
2017-05-27 10:48:03 +0300

    Correct coach model.

--- coaches/models.py
+++ coaches/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Coach(models.Model):
     user = models.OneToOneField(User)
     date_of_birth = models.DateField(verbose_name='Дата рождения')
-    gender = models.CharField(verbose_name='Пол', choices=(('M', 'Мужской'), ('W', 'Женский')), max_length=1)
+    gender = models.CharField(verbose_name='Пол', choices=(('M', 'Male'), ('F', 'Female')), max_length=1)
     phone = models.CharField(verbose_name='Телефон', max_length=15)
     address = models.CharField(verbose_name='Адрес', max_length=100)
     skype = models.CharField(max_length=20)
