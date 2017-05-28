d516bd438ef6bf131f6c9aa4382d315cc9b42fd5
Stas_Bulkin <bulkin.stanislav@omega-auto.biz>
2017-05-28 21:29:08 +0300

    it work

--- coaches/urls.py
+++ coaches/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from coaches.views import detail
 
 app_name = 'coaches'
-urlpatterns = [
+urlpatterns = (
     url(r'^(?P<param>\d+)/$', detail, name='detail'),
-]
+)
