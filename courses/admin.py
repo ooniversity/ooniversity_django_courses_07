22648543d54fd5202a046a97928e691ab0d22f70
fon_harry <fon.harry@gmail.com>
2017-05-28 23:18:10 +0300

    work 5_2

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from courses.models import Course, Lesson
 
-from .models import Course, Lesson
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    fields = ["subject", 'description', 'order']
 
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'short_description']
+    search_fields = ['name']
+    inlines = [LessonInline]
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
