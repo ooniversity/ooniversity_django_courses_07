60535b470e3fc548e0aecd6c5bf16a9b18824b8e
Eugene K <eugenek@nanorep.com>
2017-05-28 18:37:59 +0300

    Added coaches

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from coaches.models import Coach
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
     short_description = models.CharField(max_length=255)
     description = models.TextField()
+    coach = models.ForeignKey(Coach, null=True, blank=True, related_name='coach_courses')
+    assistant = models.ForeignKey(Coach, null=True, blank=True, related_name='assistant_courses')
     
     def __str__(self):
         return self.name
--- courses/views.py
+++ courses/views.py
@@ -4,5 +4,15 @@ from courses.models import Course, Lesson
 
 def detail(request, course_id):
     course_inf = Course.objects.get(id=course_id)
+    trainer = {'f_name': course_inf.coach.user.first_name, 
+                'l_name': course_inf.coach.user.last_name,
+                'descr': course_inf.coach.description,
+                'id': course_inf.coach.id}
+    assistant = {'f_name': course_inf.assistant.user.first_name, 
+                'l_name': course_inf.assistant.user.last_name,
+                'descr': course_inf.assistant.description,
+                'id': course_inf.assistant.id}
+    print(assistant)
     course_plan = Lesson.objects.filter(course=course_id)
-    return render(request, 'courses/detail.html', {'course_inf': course_inf, 'course_plan': course_plan})
+    return render(request, 'courses/detail.html', {'course_inf': course_inf, 'course_plan': course_plan,
+                                                    'trainer': trainer, 'assistant': assistant})
