dee0ce449f714d429ec38d251abf5eef1d86f0a5
Evgeniy <lepiloff82@gmail.com>
2017-05-28 21:13:30 +0300

    Add files via upload

--- students/urls.py
+++ students/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'students'
 urlpatterns = [
     url(r'^(?P<student_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^$', views.list_view, name = 'list_view'),
+    url(r'^$', views.list_view, name='list_view'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from students.models import Student
+from courses.models import Course
 
 
 def detail(request, student_id):
     student_info = Student.objects.get(id=student_id)
-    courses_list = student_info.courses.all()
-    return render(request, 'students/detail.html', {'student_info': student_info, 'courses_list': courses_list})
+    return render(request, 'students/detail.html', {'student_info': student_info})
     
-def list_view(request): 
-
-    course_id = request.GET.get('course_id', '')
-    
-    if course_id == '':
-        student_list = Student.objects.all()
+def list_view(request):
+    without_c = False
+    try:
+        course_id = request.GET['course_id']
+    except KeyError:
+        without_c = True
+        
+    if without_c == True:
+        students_list = Student.objects.all
+        active_course = None
     else:
-        student_list = Student.objects.filter(courses=course_id)
-
-    return render(request, "students/list.html", {"student_list": student_list })
-
-# Create your views here.
+        students_list = Student.objects.filter(courses=course_id)
+        active_course = Course.objects.get(id=course_id)
+    
+    return render(request, 'students/list.html', {'st_list': students_list})
