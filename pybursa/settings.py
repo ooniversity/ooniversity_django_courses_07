5f7da0c02f4af3e28c779a956e8d2fd618a450fc
Evgeniy <lepiloff82@gmail.com>
2017-05-28 21:12:49 +0300

    Add files via upload

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'courses',
     'students',
+    'coaches',
 ]
 
 MIDDLEWARE = [
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,16 +1,17 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
-from quadratic.views import quadratic_results
 
 urlpatterns = [
-    url(r'^', include('courses.urls')),
-    url(r'^students/', include('students.urls')),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^student_list/$', views.student_list, name='student_list'),
-    url(r'^student_detail/$', views.student_detail, name='student_detail'),
-    url(r'^quadratic/results/$', quadratic_results, name='quadratic_results'), 
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+	url(r'^$', views.index, name='index'),
+	url(r'^contact/$', views.contact, name='contact'),
+	url(r'^student_list/$', views.student_list, name='student_list'),
+	url(r'^student_detail/$', views.student_detail, name='student_detail'),
+        url(r'^admin/', admin.site.urls),	
+	url(r'^polls/', include('polls.urls')),
+	url(r'^quadratic/', include('quadratic.urls')),
+	url(r'^courses/', include('courses.urls')),
+	url(r'^students/', include('students.urls')),
+	url(r'^coaches/', include('coaches.urls')),
 ]
 admin.site.site_header = 'PyBursa Administration'
