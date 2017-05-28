c91d66fc250a006820101f62d88323d2fabaa813
Stas_Bulkin <bulkin.stanislav@omega-auto.biz>
2017-05-28 17:08:22 +0300

    new var

--- courses/admin.py
+++ courses/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
-#from django.db import models
-from courses.models import Course, Lesson
+
+from .models import Course, Lesson
+
+
+class LessonInline(admin.TabularInline):
+	model = Lesson
+	extra = 0
+
 
 class CourseAdmin(admin.ModelAdmin):
-	list_display = ["name", "short_description", "description"]
+	list_display = ['name', 'short_description']
+	search_fields = ['name']
+	inlines = [LessonInline]
+
 
 class LessonAdmin(admin.ModelAdmin):
-	list_display = ["subject", "description", "course", "order"]
-
-#class StudentAdmin(admin.ModelAdmin):
-#	list_display = ["name", "surname", "date_of_birth", "email", "phone", "adress", "skype", "get_courses"]
-
+	list_display = ('subject', 'description')
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-#admin.site.register(Student, StudentAdmin)
-# Register your models here.
+
+
+
+
--- courses/views.py
+++ courses/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Course,Lesson
-from  django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render
+from courses.models import Course, Lesson
 
-def get_all_courses_info(request):
-    our_courses = Course.objects.all()
-    template = 'index.html'
-    return render(request,(template),{"courses_list":our_courses})
 
-def detail(request,course_id):
-    course = get_object_or_404(Course,pk=course_id)
-    template = 'courses/detail.html'
-    lessons_per_course = Lesson.objects.filter(course__id__exact=course.id).order_by('order')
-    context = {"lessons_list":lessons_per_course,'course':course}
-return render(request,template,context)
-# Create your views here.
+def index(request):
+	cour = Course.objects.all()
+	context = {"cour": cour}
+	return render(request, 'index.html', context)
+
+
+def detail(request, pk):
+	one_c = Course.objects.get(id__exact = int(pk))
+	lessons = Lesson.objects.filter(course__id__exact = int(pk))
+	context = {"course": one_c, "lessons": lessons}
+	return render(request, 'courses/detail.html', context)
