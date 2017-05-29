801ceb674e61a80eeec5cac6298fc4addb6cebfc
Evgeniy <lepiloff82@gmail.com>
2017-05-29 21:32:46 +0300

    Add files via upload

--- courses/views.py
+++ courses/views.py
@@ -3,18 +3,18 @@ from courses.models import Course, Lesson
 
 
 def detail(request, course_id):
-    course_inf = Course.objects.get(id=course_id)
-    teacher = {'f_name': course_inf.coach.user.first_name, 
-                'l_name': course_inf.coach.user.last_name,
-                'descr': course_inf.coach.description,
-                'id': course_inf.coach.id}
-    assistant = {'f_name': course_inf.assistant.user.first_name, 
-                'l_name': course_inf.assistant.user.last_name,
-                'descr': course_inf.assistant.description,
-                'id': course_inf.assistant.id}
-    print(assistant)
+    course_info = Course.objects.get(id=course_id)
+    teacher = {'first_name': course_info.coach.user.first_name, 
+                'last_name': course_info.coach.user.last_name,
+                'description': course_info.coach.description,
+                'id': course_info.coach.id}
+    assistant = {'first_name': course_info.assistant.user.first_name, 
+                'last_name': course_info.assistant.user.last_name,
+                'description': course_info.assistant.description,
+                'id': course_info.assistant.id}
+    
     course_plan = Lesson.objects.filter(course=course_id)
-    return render(request, 'courses/detail.html', {'course_inf': course_inf, 'course_plan': course_plan,
+    return render(request, 'courses/detail.html', {'course_info': course_info, 'course_plan': course_plan,
                                                     'teacher': teacher, 'assistant': assistant})
 
 
