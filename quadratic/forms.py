fc3a8f0fbea7547b2a1f48c173b87eb4a7e69635
kpekep <link2vkim@gmail.com>
2017-06-01 13:46:18 +0300

    KZ7.2v12

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -2,11 +2,14 @@ from django import forms
 
 
 class QuadraticForm(forms.Form):
-    a = forms.IntegerField(localize=True, label='коэффициент a')
-    b = forms.IntegerField(localize=True,label='коэффициент b')
-    c = forms.IntegerField(localize=True, label='коэффициент c')
+    # a = forms.IntegerField(localize=True, label='коэффициент a')
+    # b = forms.IntegerField(localize=True, label='коэффициент b')
+    # c = forms.IntegerField(localize=True, label='коэффициент c')
+    a = forms.CharField(label='коэффициент a')
+    b = forms.CharField(label='коэффициент b')
+    c = forms.CharField(label='коэффициент c')
 
     def clean_a(self):
         data = self.cleaned_data['a']
-        if data is 0:
+        if data is '0':
             raise forms.ValidationError('коэффициент при первом слагаемом уравнения не может быть равным нулю.')
