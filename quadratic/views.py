f3732dd8ed34828bd17a39f3db85ff5007b9ee04
kpekep <link2vkim@gmail.com>
2017-06-01 04:17:06 +0300

    KZ7.2v1

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,60 +1,41 @@
 from django.shortcuts import render
-from django import forms
+from .forms import QuadraticForm
 
 
-class QuadraticForm(forms.Form):
-	a = forms.CharField(max_length=10)
-	b = forms.CharField(max_length=10)
-	c = forms.CharField(max_length=10)
-
+def check(val):
+	if val is None:
+		output = None
+	else:
+		try:
+			output = int(val)
+		except:
+			output = 'not_int'
+	return output
 
 def quadratic_results(request):
-
-	context = {}
-
-	form = QuadraticForm()
-	context['form'] = form
-
-	values = {}
-	messages = {}
-
-	for arg in request.GET:
-	
-		try:
-			values[arg] = int(request.GET[arg])
-		except ValueError:
-			if not request.GET[arg]:
-				messages[arg] = 'коэффициент не определен'
-			elif not request.GET[arg].replace('-','').isdigit():
-				messages[arg] = 'коэффициент не целое число'
-
-			values[arg] = request.GET[arg]
-
-	if 'a' in values and values['a'] == 0:
-		messages['a'] = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
-
-	if len(messages) == 0:
-		desc = values['b']**2 - 4*values['a']*values['c']
-		values['desc'] = 'Дискриминант: ' + str(desc)
-
+	errors = False
+	form = QuadraticForm(request.GET)
+	context = {'form': form}
+	for key in ['a', 'b', 'c']:
+		context[key] = check(request.GET.get(key))
+		if context[key] is None:
+			errors = True
+		if context[key] is 'not_int':
+			context['err_' + key] = 'коэффициент не целое число'
+			errors = True
+	if not errors:
+		a = context['a']
+		b = context['b']
+		c = context['c']
+		desc = b**2 - 4*a*c
+		context['desc'] = 'Дискриминант: %d' % desc
 		if desc < 0:
-
-			result = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
-
+			context['result'] = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
 		elif desc == 0:
-
-			x = -values['b']/(2*values['a'])
-			result = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {}'.format(x)
-
+			x = -b/(2*a)
+			context['result'] = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {}'.format(x)
 		elif desc > 0:
-
-			x1 = (-values['b'] + desc**0.5)/(2*values['a'])
-			x2 = (-values['b'] - desc**0.5)/(2*values['a'])
-			result = 'Квадратное уравнение имеет два действительных корня: x1 = {0}, x2 = {1}'.format(x1, x2)
-
-		messages['result'] = result
-
-	context['values'] = values
-	context['messages'] = messages
-
+			x1 = (-b + desc**0.5)/(2*a)
+			x2 = (-b - desc**0.5)/(2*a)
+			context['result'] = 'Квадратное уравнение имеет два действительных корня: x1 = {0}, x2 = {1}'.format(x1, x2)
 	return render(request, 'result.html', context)
