90e9402c68c11d9b3bc1ace19a4ec93eb46fa125
kpekep <link2vkim@gmail.com>
2017-06-01 16:53:20 +0300

    KZ7.2v16

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -7,6 +7,7 @@ class QuadraticForm(forms.Form):
     c = forms.IntegerField(localize=True, label='коэффициент c')
 
     def clean_a(self):
-        data = self.cleaned_data['a']
-        if data is 0:
+        a = self.cleaned_data['a']
+        if a == 0:
             raise forms.ValidationError('коэффициент при первом слагаемом уравнения не может быть равным нулю')
+        return a
