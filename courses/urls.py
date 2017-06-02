382fec5abeb814fbe80d6457a2e165a03c45cc5a
Yuriy <garazdcreation@gmail.com>
2017-06-02 16:35:44 +0300

    KZ-7

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
-from courses.views import index, detail
+from courses.views import list_view, detail
 
 app_name = 'courses'
 urlpatterns = [
-    url(r'^$', index, name='index'),
+    url(r'^$', list_view, name='list_view'),
     url(r'^courses/(?P<course_id>[0-9]+)/$', detail, name='detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 from courses.models import Course, Lesson
 
 
-def index(request): 
+def list_view(request):
 
     courses_list = Course.objects.all()
-    return render(request, "courses/index.html", {"courses_list": courses_list})
+    return render(request, "courses/list.html", {"courses_list": courses_list})
 
 
-def detail(request, course_id): 
+def detail(request, course_id):
 
     course_obj = Course.objects.get(id=course_id)
     #lessons_list = course_obj.lesson_set.all()  #course_obj.lesson_set.all
     lessons_list = Lesson.objects.filter(course=course_id)
     return render(request, "courses/detail.html", {"course": course_obj, "lessons_list": lessons_list })
-
