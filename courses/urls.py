0fafe960e74829f3dd7bcc258a971b54b99de2c8
mishabeliy15 <misha.beliy15@gmail.com>
2017-06-02 19:29:55 +0300

    KZ 8

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from . import views
+from courses.views import detail,create,edit, remove,add_lesson
 
 app_name='courses'
 urlpatterns = [
-    url(r'^(?P<course_id>\d+)/$',views.detail,name='detail'),
+    url(r'^(?P<course_id>\d+)/$',detail,name='detail'),
+    url(r'^add/$',create,name='add'),
+    url(r'^edit/(?P<course_id>\d+)/$',edit,name='edit'),
+    url(r'^remove/(?P<course_id>\d+)/$',remove,name='remove'),
+    url(r'^(?P<course_id>\d+)/add_lesson/$',add_lesson,name='add-lesson'),
 ]
