64270bcb6ec8fb80ff18877632cdaf05dd62fdfc
Evgeniy <lepiloff82@gmail.com>
2017-06-03 19:02:39 +0300

    Add files via upload

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class QuadraticForm(forms.Form):
-    a = forms.CharField(max_lenght = 100)
-    b = forms.CharField(max_lenght = 100)
-    c = forms.CharField(max_lenght = 100)
+    a = forms.IntegerField(label='коэффициент a')
+    b = forms.IntegerField(label='коэффициент b')
+    c = forms.IntegerField(label='коэффициент c')
+    
+    def clean_a(self):
+        a = self.cleaned_data['a']
+        if a == 0:
+            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        return a
