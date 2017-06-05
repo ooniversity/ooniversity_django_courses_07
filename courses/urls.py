f3316acfdf7a9f87b4ab12f990f7334f36047ae2
tarchanskyj <ihp.arthur@gmail.com>
2017-06-05 23:32:09 +0300

    kr 7 3

--- courses/urls.py
+++ courses/urls.py
@@ -6,7 +6,7 @@ app_name = 'courses'
 urlpatterns = [
     url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<course_id>[0-9]+)/add_lesson/$', views.add_lesson, name='add_lesson'),
-    url(r'^add/$', views.create, name='add'),
+    url(r'^add/$', views.add, name='add'),
     url(r'^edit/(?P<course_id>[0-9]+)/$', views.edit, name='edit'),
     url(r'^remove/(?P<course_id>[0-9]+)/$', views.remove, name='remove'),
 ]
