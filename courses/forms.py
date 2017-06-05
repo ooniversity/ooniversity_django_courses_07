552c1f8dfc3a9d8ad838cafe6673b0308b354cee
Stas_Bulkin <bulkin.stanislav@omega-auto.biz>
2017-06-05 16:14:45 +0300

    add templates

---
+++ courses/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Course, Lesson
+
+
+class CourseModelForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+class LessonModelForm(forms.ModelForm):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
