aeb31611b923802fc97f4175f8bfb7a848d20edf
tarchanskyj <ihp.arthur@gmail.com>
2017-06-05 23:49:01 +0300

    kr 7 3

--- courses/urls.py
+++ courses/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^add/$', views.add, name='add'),
     url(r'^edit/(?P<course_id>[0-9]+)/$', views.edit, name='edit'),
     url(r'^remove/(?P<course_id>[0-9]+)/$', views.remove, name='remove'),
-    url(r'^(?P<course_id>[0-9]+)/add_lesson/$', views.add_lesson, name='add_lesson'),
+    url(r'^(?P<course_id>[0-9]+)/add-lesson/$', views.add_lesson, name='add_lesson'),
 ]
