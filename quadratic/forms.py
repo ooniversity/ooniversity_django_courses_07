20b6e6f40ac1ba9865a34c2c590cd2d70b1dfa4f
Eugene K <eugenek@nanorep.com>
2017-06-06 17:39:38 +0300

    Fix function name

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -5,7 +5,7 @@ class QuadraticForm(forms.Form):
     b = forms.IntegerField(label='коэффициент b')
     c = forms.IntegerField(label='коэффициент c')
     
-    def cleandata_a(self):
+    def clean_a(self):
         data = self.cleaned_data['a']
         if data == 0:
             raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
