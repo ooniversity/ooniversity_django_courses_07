9656e6d7521ae0d1b52bd548cec1cf7d3291b442
Valerii <ponomarenko.valerij@gmai.com>
2017-06-07 22:02:32 +0300

    add kr9_2

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-from courses.views import detail, add, edit, remove, add_lesson
+from courses import views
 
 app_name = 'courses'
 urlpatterns = (
-    url(r'^(?P<pk>\d+)/$', detail, name='detail'),
-    url(r'^add/$', add, name='add'),
-    url(r'^edit/(?P<id>\d+)/$', edit, name='edit'),
-    url(r'^remove/(?P<id>\d+)/$', remove, name='remove'),
-    url(r'^(?P<id>\d+)/add_lesson$', add_lesson, name='add-lesson'),
+    url(r'^(?P<pk>\d+)/$', views.CourseDetailView.as_view(), name='detail'),
+    url(r'^add/$', views.CourseCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.CourseUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.CourseDeleteView.as_view(), name='remove'),
+    url(r'^(?P<id>\d+)/add_lesson$', views.add_lesson, name='add-lesson'),
 )
