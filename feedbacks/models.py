3f84db9d9945d8ec4fac85fd7352f015f0f1721c
kpekep <link2vkim@gmail.com>
2017-06-09 03:27:21 +0300

    10.1v1

---
+++ feedbacks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Feedback(models.Model):
+    subject = models.CharField(max_length=100)
+    from_email = models.EmailField()
+    name = models.CharField(max_length=50)
+    message = models.TextField()
+    create_date = models.DateTimeField(auto_now=True)
---
+++ feedbacks/views.py
@@ -0,0 +1,25 @@
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.core.mail import mail_admins
+from feedbacks.models import Feedback
+from feedbacks.forms import FeedbackModelForm
+
+
+class FeedbackCreateView(CreateView):
+    model = Feedback
+    form_class = FeedbackModelForm
+    template_name = 'feedback.html'
+    context_object_name = 'feedback'
+    success_url = reverse_lazy('feedback')
+
+    def form_valid(self, form):
+        feedback = form.cleaned_data
+        mail_admins(
+            feedback.get('subject'), 
+            feedback.get('message'),
+            fail_silently=False
+            )
+        messages.success(self.request, 
+            'Thank you for your feedback! We will keep in touch with you very soon!')
+        return super().form_valid(form)
