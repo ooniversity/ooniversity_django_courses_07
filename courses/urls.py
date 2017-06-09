c7ad87f59474470eab0ff92a7b7546b756192af1
Vasilev A <crawless@bk.ru>
2017-06-09 21:09:04 +0300

    Task #9_2

--- courses/urls.py
+++ courses/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'courses'
 urlpatterns = [
     url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^add/', views.create, name='add'),
-    url(r'^edit/(?P<course_id>[0-9]+)/', views.edit, name='edit'),
-    url(r'^remove/(?P<course_id>[0-9]+)/', views.remove, name='remove'),
+    url(r'^add/', views.CourseCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<course_id>[0-9]+)/', views.CourseUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<course_id>[0-9]+)/', views.CourseDeleteView.as_view(), name='remove'),
     url(r'^(?P<course_id>[0-9]+)/add_lesson$', views.add_lesson, name='add-lesson'),
 ]
