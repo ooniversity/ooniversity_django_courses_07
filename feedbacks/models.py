362c5f0589b4646dc51399048d40ab300a955e2c
kpekep <link2vkim@gmail.com>
2017-06-09 03:41:19 +0300

    10.1v3

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class Feedback(models.Model):
-    subject = models.CharField(max_length=100)
-    from_email = models.EmailField()
     name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=100)
     message = models.TextField()
-    create_date = models.DateTimeField(auto_now=True, editable=False)
+    from_email = models.EmailField()
+    create_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
