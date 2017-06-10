05f03c24760fdd0b62e3ebe0d6fb5309e758d50e
Evgeniy <lepiloff82@gmail.com>
2017-06-10 17:45:40 +0300

    Add files via upload

--- courses/urls.py
+++ courses/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'courses'
 urlpatterns = [
-    url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^add/$', views.add, name='add'),
-    url(r'^edit/(?P<id>\d+)/$', views.edit, name='edit'),
-    url(r'^remove/(?P<id>\d+)/$', views.remove, name='remove'),
+    url(r'^(?P<pk>\d+)/$', views.CourseDetailView.as_view(), name='detail'),
+    url(r'^add/$', views.CourseCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.CourseUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.CourseDeleteView.as_view(), name='remove'),
     url(r'^(?P<id>\d+)/add_lesson/$', views.add_lesson, name='add-lesson'),
 ]
