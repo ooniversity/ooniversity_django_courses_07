54bc4e5bb28e4bb568150b5804eedd5075aee151
Yuriy <garazdcreation@gmail.com>
2017-06-11 16:43:44 +0300

    Add SuccessMessageMixin

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class Feedback(models.Model):
     name = models.CharField(max_length=128)
-    subject = models.CharField(max_length=254)
-    message = models.TextField(null=True)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
     from_email = models.EmailField(unique=True)
     create_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.subject
 
