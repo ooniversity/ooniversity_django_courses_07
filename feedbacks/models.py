96f4f29fbe38caa3967508d43b6b3970359768f5
Vasilev A <crawless@bk.ru>
2017-06-11 16:36:18 +0300

    Task #10_1 FIX 01

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Feedback(models.Model):
     name = models.CharField(max_length=64)
     subject = models.CharField(max_length=64)
     message = models.TextField()
     from_email = models.EmailField()
-    create_date = models.DateField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
     
