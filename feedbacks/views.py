5b8d5074063104fd1f31dcb2be75d2788ae35acd
Yuriy <garazdcreation@gmail.com>
2017-06-11 17:22:45 +0300

    Fix messages

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -6,19 +6,17 @@ from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.conf import settings
-from django.contrib.messages.views import SuccessMessageMixin
 
 
-class FeedbackView(SuccessMessageMixin, CreateView):
+class FeedbackView(CreateView):
     model = Feedback
-    #form_class = FeedbackForm
-    fields = ['name', 'subject', 'message', 'from_email']
+    form_class = FeedbackForm
     template_name = 'feedback.html'
     success_url = reverse_lazy('feedback')
-    success_message = 'Thank you for your feedback! We will keep in touch with you very soon!'
 
     def form_valid(self, form):
         response = super().form_valid(form)
         send_mail(form.instance.subject, form.instance.message, form.instance.from_email, settings.ADMINS, fail_silently=False)
+        messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!')
         return response
 
