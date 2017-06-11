11673829efac6164b8d7c39edbe04d91394e3a29
Yuriy <garazdcreation@gmail.com>
2017-06-11 16:55:13 +0300

    Fix DateTime field

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Feedback(models.Model):
     name = models.CharField(max_length=128)
     subject = models.CharField(max_length=255)
-    message = models.TextField()
+    message = models.TextField(null=True)
     from_email = models.EmailField(unique=True)
-    create_date = models.DateField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.subject
