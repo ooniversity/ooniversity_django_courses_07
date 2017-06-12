2c3c3bdb7b5d0257af48ebfb945b801d91f135c6
Eugene K <klaus@itsyndicate.org>
2017-06-12 23:17:35 +0300

    Task 9_2

--- coaches/urls.py
+++ coaches/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'coaches'
+
 urlpatterns = [
-    url(r'^(?P<coach_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
 ]
+
--- students/urls.py
+++ students/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from students import views
+
+from . import views
 
 app_name = 'students'
-urlpatterns = (
+
+urlpatterns = [
     url(r'^$', views.StudentListView.as_view(), name='list_view'),
     url(r'^(?P<pk>\d+)/$', views.StudentDetailView.as_view(), name='detail'),
     url(r'^add/$', views.StudentCreateView.as_view(), name='add'),
     url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit'),
     url(r'^remove/(?P<pk>\d+)/$', views.StudentDeleteView.as_view(), name='remove'),
-)
+]
