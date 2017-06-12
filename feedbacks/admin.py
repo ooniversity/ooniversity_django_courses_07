70ea89fe5e6957dfbe5fe255255a817d7dc8f033
mishabeliy15 <misha.beliy15@gmail.com>
2017-06-12 14:59:01 +0300

    kz10_add admin class

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Feedback
 
-admin.site.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'from_email')
+    list_display = ['from_email', 'create_date']
+    list_filter = ['create_date']
+
+admin.site.register(Feedback,FeedbackAdmin)
