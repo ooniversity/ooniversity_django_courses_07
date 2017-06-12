3165aba7ccdf43df5eeb4f80150271a3f132f8d8
Yuriy <garazdcreation@gmail.com>
2017-06-12 23:40:44 +0300

    FIX KZ10-1

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -11,11 +11,16 @@ from django.conf import settings
 class FeedbackView(CreateView):
     model = Feedback
     form_class = FeedbackForm
-    template_name = 'feedback.html'
     success_url = reverse_lazy('feedback')
+    template_name = 'feedback.html'
 
     def form_valid(self, form):
         message_to_admins = 'From: ' + form.cleaned_data['name'] + '(' +form.cleaned_data['from_email'] + ')\n' + form.cleaned_data['subject'] + '\n' + form.cleaned_data['message']
         mail_admins('Feedback', message_to_admins)
         messages.success(self.request, 'Thank you for your feedback! We will keep in touch with you very soon!')
         return super(FeedbackView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(FeedbackView, self).get_context_data(**kwargs)
+        context['title'] = "Leave a feedback"
+        return context
