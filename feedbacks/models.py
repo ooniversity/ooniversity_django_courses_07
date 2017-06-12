b74c612602b4e389f9ae991aac74dc2e6e1e8df4
tarchanskyj <ihp.arthur@gmail.com>
2017-06-12 21:30:35 +0300

    KZ-10-1 upd 1

---
+++ feedbacks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    from_email = models.EmailField()
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
