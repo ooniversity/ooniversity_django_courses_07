ae0705527c04c4f7d897a82617de2fcee090f3bd
Eugene K <eugenek@nanorep.com>
2017-06-13 16:46:39 +0300

    Added files

---
+++ pybursa/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from courses.models import Course
+
+
+def index(request):
+    course_list = Course.objects.all()
+    return render(request, 'index.html', {'course_list': course_list})
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def student_list(request):
+    return render(request, "student_list.html")
+
+
+def student_detail(request):
+    return render(request, "student_detail.html")
