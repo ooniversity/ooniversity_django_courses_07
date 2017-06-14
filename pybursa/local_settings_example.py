ffc0f0efdfa3712d8916fd32b827402b9e053909
Valerii <ponomarenko.valerij@gmai.com>
2017-06-14 18:04:33 +0300

    add kr12_1_1

--- pybursa/local_settings_example.py
+++ pybursa/local_settings_example.py
@@ -23,61 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1b_fjyt!(&yw^y^xl@w-9-)&@ho^l7m6=j_#y^)drnx4a&0*83'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'quadratic',
-    'courses',
-    'students',
-    'coaches',
-    'feedbacks',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'pybursa.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 WSGI_APPLICATION = 'pybursa.wsgi.application'
-
-
-APPEND_SLASH = False
 
 
 # Database
@@ -109,31 +60,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Europe/Kiev'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 ADMINS = (('Ponomarenko Valerii', 'ponomarenko.valerij@gmail.com'), ('Valerii Ponomarenko', 'panval@mail.ru'))
 
@@ -188,9 +114,3 @@ LOGGING = {
         },
     },
 }
-
-
-try:
-    from .local_settings_example import *
-except ImportError:
-    print("Warning! local_settings are not defined!")
