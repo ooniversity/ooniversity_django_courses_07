b8f0dfe6f1c07e351e0535ecd15d0629cb2ea007
Evgeniy <lepiloff82@gmail.com>
2017-06-15 20:21:17 +0300

    Add files via upload

--- students/views.py
+++ students/views.py
@@ -5,12 +5,23 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 
 
 class StudentDetailView(DetailView):
     model = Student
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        logger.debug("Students detail view has been debugged!")
+        logger.info("Logger of students detail view informs you!")
+        logger.warning("Logger of students detail view warns you!")
+        logger.error("Students detail view went wrong!")
+        return context
     
 
 class StudentListView(ListView):
