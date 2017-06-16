9e545e19c48afe162e64ce66790e0ac18f1d9466
Vasilev A <crawless@bk.ru>
2017-06-16 13:26:08 +0300

    Task #11_2

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from courses.models import Course
 
-# Create your tests here.
+class CoursesListTest(TestCase):
+    def test_response(self):
+        from django.test import Client
+        client = Client()
+
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class CoursesDetailTest(TestCase):
+    def test_course_create(self):
+        course1 = Course.objects.create(
+            name = 'Course1',
+            short_description = 'test',
+            description = '1',
+        )
+        self.assertEqual(Course.objects.all().count(), 1)
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from students.models import Student
+import datetime
 
-# Create your tests here.
+
+class StudentsListTest(TestCase):
+    def test_response(self):
+        from django.test import Client
+        client = Client()
+
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+
+
+class StudentsDetailTest(TestCase):
+    def test_student_create(self):
+        student1 = Student.objects.create(
+            name = 'Student1',
+            surname = 'Test',
+            date_of_birth = datetime.date.today(),
+        )
+        self.assertEqual(Student.objects.all().count(), 1)
