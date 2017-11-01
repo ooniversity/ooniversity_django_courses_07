0f2f35a9716f4c0021a31a72a2e55ffbbf2a1d69
Roman <Roman@RAsus>
2017-11-02 01:28:34 +0300

    ..

--- students/urls.py
+++ students/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'students'
 urlpatterns = [
-    url(r'^$', views.list_view, name='list'),
+    url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<student_id>\d+)/$', views.detail, name='detail')
 ]
