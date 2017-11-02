8a8a713680e8d6041fab37b1b3f0b2288dbe640f
Roman <Roman@RAsus>
2017-11-03 01:42:46 +0300

    ..

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,22 +1,20 @@
 from django import forms
-from django.core.exceptions import ValidationError
+from django.core import validators
 
-
-def validate_all(value):
-    try:
-        value = int(value)
-    except:
-        raise ValidationError('коэффициент не целое число')
 
 class QuadraticForm(forms.Form):        
-    a = forms.CharField(label='коэффициент a', max_length=10, validators=[validate_all])
-    b = forms.CharField(label='коэффициент b', max_length=10, validators=[validate_all])
-    c = forms.CharField(label='коэффициент c', max_length=10, validators=[validate_all])
+    a = forms.CharField(label='коэффициент a', max_length=10, validators=[validators.int_list_validator('')])
+    b = forms.CharField(label='коэффициент b', max_length=10, validators=[validators.int_list_validator('')])
+    c = forms.CharField(label='коэффициент c', max_length=10, validators=[validators.int_list_validator('')])
        
     
     def clean_a(self):
         data = self.cleaned_data['a']
-        if int(data) == 0:
-            raise forms.ValidationError('коэффициент при первом слагаемом уравнения не может быть равным нулю')
+        try:
+            data = int(data)
+            if int(data) == 0:
+                raise forms.ValidationError('коэффициент при первом слагаемом уравнения не может быть равным нулю')
+        except:
+            raise forms.ValidationError('коэффициент не целое число')
         
         return data
