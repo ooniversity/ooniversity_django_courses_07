2ddfffba6485def5a809f1972442f27c8b783890
Roman <Roman@RAsus>
2017-11-03 03:11:43 +0300

    .8

--- quadratic/views.py
+++ quadratic/views.py
@@ -4,7 +4,6 @@ import math
 
 # Create your views here.
 def quadratic_results(request):
-    x1 = x2 = None
     form = QuadraticForm()
     if request.GET:
             form = QuadraticForm(request.GET)
@@ -16,15 +15,16 @@ def quadratic_results(request):
         b = form.cleaned_data['b']
         c = form.cleaned_data['c']
         d = b**2 - 4*a*c
+        context['d'] = 'Дискриминант: {}'.format(d)
          
-        if d == 0:
-            x1 = x2 = -b / 2*a
+        if d < 0:
+            context['result'] = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
+        elif d == 0:
+            x = -b / 2*a
+            context['result'] = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {}'.format(x)
         elif d > 0:
             x1 = (-b + math.sqrt(d)) / 2*a
             x2 = (-b - math.sqrt(d)) / 2*a
-            
-        context['d'] = d
-        context['x1'] = x1
-        context['x2'] = x2            
+            context['result'] = 'Квадратное уравнение имеет два действительных корня: x1 = {}, x2 = {}'.format(x1, x2)         
     
     return render(request, 'quadratic/results.html', context)
