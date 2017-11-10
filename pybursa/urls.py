fb8c4fdcd4eb7a9dff7805e97446954fe542cda0
Anton Steshenko <bpmnsoftware@gmail.com>
2017-11-10 22:57:10 +0200

    last work

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,8 +1,11 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.shortcuts import render
+
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
 ]
