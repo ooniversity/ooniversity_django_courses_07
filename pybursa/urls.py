128f751b558c6c8eb6d1bb63ac7beebfc979f3f9
Anton Steshenko <bpmnsoftware@gmail.com>
2017-11-12 14:24:57 +0200

    Added urls and static

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -2,10 +2,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.shortcuts import render
+from .views import index, contact, student_list, student_detail
 
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
-    url(r'^$', views.index),
+    url(r'^$', index, name = 'index'),
+    url(r'^contact/$', contact, name = 'contact'),
+    url(r'^student_list/$', student_list, name = 'student_list'),
+    url(r'^student_detail/$', student_detail, name = 'student_detail'),
     url(r'^admin/', admin.site.urls),
 ]
