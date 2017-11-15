a4224c8b2ce5831fea73427726fca1db9a7c1924
Anton Steshenko <bpmnsoftware@gmail.com>
2017-11-15 22:51:34 +0200

    errors corrected 5

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -3,18 +3,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.shortcuts import render
 from .views import contact, student_list, student_detail
-from courses.views import index, detail
-
+from courses.views import index
 
 
 urlpatterns = [
+    url(r'^$', index, name = 'index'),
     url(r'^quadratic/', include('quadratic.urls')),
     url(r'^polls/', include('polls.urls')),
-    url(r'^$', index, name = 'index'),
-    url(r'^(?P<cd_id>[0-9]+)/$', detail, name = 'detail'),
-    url(r'^students/', include('students.urls')),
     url(r'^contact/$', contact, name = 'contact'),
+    url(r'^students/', include('students.urls')),
+    url(r'^courses/', include('courses.urls')),
     url(r'^student_list/$', student_list, name = 'student_list'),
     url(r'^student_detail/$', student_detail, name = 'student_detail'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls)
 ]
