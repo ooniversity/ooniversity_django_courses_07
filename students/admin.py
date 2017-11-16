9794e10a45019fddce06a904cbe29f4713134901
Anton Steshenko <bpmnsoftware@gmail.com>
2017-11-16 15:41:05 +0200

    control 5 part 2

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from students.models import Student
+from django.db import models
+from django.forms import widgets
 
 # Register your models here.
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Personal info',   {'fields': ['name', 'surname', 'date_of_birth']}),
+        ('Contact info',    {'fields': ['email', 'phone', 'address', 'skype']}),
+        (None,              {'fields': ['courses']})
+        ]
+    list_display = ['full_name','email', 'skype']
+    search_fields = ['surname']
+    
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': widgets.SelectMultiple}
+}
+
+admin.site.register(Student, StudentAdmin)
