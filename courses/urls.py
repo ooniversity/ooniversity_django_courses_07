2391a1a84e8c59cbcccc802323d46003e8008138
Anton Steshenko <bpmnsoftware@gmail.com>
2017-11-23 15:13:39 +0200

    Control work 9.2

--- courses/urls.py
+++ courses/urls.py
@@ -1,13 +1,13 @@
 
 from django.conf.urls import include, url
-from courses.views import detail, add, remove, edit, add_lesson
-
+from courses.views import add_lesson
+from courses import views
 app_name = 'courses'
 urlpatterns = [
-    url(r'^(?P<cd_id>[0-9]+)/$', detail, name = 'detail'),
-    url(r'^add/$', add, name = 'add'),
-    url(r'^edit/(?P<pk>[0-9]+)$', edit, name = 'edit'),
-    url(r'^remove/(?P<pk>[0-9]+)$', remove, name = 'remove'),
+    url(r'^(?P<pk>[0-9]+)/$', views.CourseDetailView.as_view(), name = 'detail'),
+    url(r'^add/$', views.CourseCreateView.as_view(), name = 'add'),
+    url(r'^edit/(?P<pk>[0-9]+)$', views.CourseUpdateView.as_view(), name = 'edit'),
+    url(r'^remove/(?P<pk>[0-9]+)$', views.CourseDeleteView.as_view(), name = 'remove'),
     url(r'^(?P<pk>[0-9]+)/add_lesson/$', add_lesson, name = 'add-lesson')
 ]
    
