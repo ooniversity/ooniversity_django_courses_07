99474da088d11902203dbec736076ee2a5e817aa
Sofiia Omelchenko <sofi.odw@gmail.com>
2018-01-11 12:08:14 +0200

    Completed third(2) task

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^polls/', include('polls.urls')),
-]
+    # url(r'^admin/', admin.site.urls),
+    # url(r'^polls/', include('polls.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^student_list/', views.student_list, name='student_list'),
+    url(r'^student_detail/$', views.student_detail, name='student_detail'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
