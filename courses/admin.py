e743d5c0845984b12066b68cd5bf031a2a841de3
Sofiia Omelchenko <sofi.odw@gmail.com>
2018-01-19 17:44:46 +0200

    Task 5.2 is done

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from courses.models import *
 
-admin.site.register(Course)
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    fields = ['subject', 'description', 'order', ]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description')
+    search_fields = ['name', ]
+    inlines = [LessonInline]
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
-  # Register your models here.
--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    # prepopulated_fields = {'full name': ('name', 'surname',), }
+    admin.site.site_header = "PyBursa Administration"
+    list_display = ( 'get_full_name','email','skype')
+    search_fields = ['surname','email', ]
+    list_filter = ['courses',]
+
+    fieldsets = (( "Personal info",
+                  {'fields': ('name', 'surname', 'date_of_birth')}),
+                 ('Contact info',
+                  {'classes': ('wide',),
+                   'fields': ('email', 'phone','address','skype', 'courses')}),
+
+                 )
+
+
+admin.site.register(Student, StudentAdmin)
 
