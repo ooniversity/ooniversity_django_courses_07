5d90167897f6937d58ccd498336c71f44f460cdf
Sofiia Omelchenko <sofi.odw@gmail.com>
2018-01-23 22:32:38 +0200

    Change title

--- coaches/views.py
+++ coaches/views.py
@@ -8,7 +8,7 @@ class DetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['assistant_list']= Coach.objects.all().filter(assistant_courses = self.object)
-        context['coach_list']= Coach.objects.all().filter(coach_courses = self.object)
+        context['assistant_list']= Course.objects.all().filter(assistant_id = self.object)
+        context['coach_list']= Course.objects.all().filter(coach_id = self.object)
         return context
 
