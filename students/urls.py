dbf689e36553a312cae7c24625d49a962ac91b8b
Sofiia Omelchenko <sofi.odw@gmail.com>
2018-01-29 19:04:58 +0200

    Changes in View

--- students/urls.py
+++ students/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'add/$', views.StudentCreateView.as_view() , name='create'),
     url(r'edit/(?P<pk>[0-9]+)/$', views.StudentUpdateView.as_view(), name='edit'),
     url(r'remove/(?P<pk>[0-9]+)/$', views.StudentDeleteView.as_view() , name='remove'),
-    url(r'^$', views.StudentListView.as_view(), name = 'list'),
+    url(r'^$', views.StudentListView.as_view(), name = 'list_view'),
     url(r'^(?P<pk>[0-9]+)/$', views.StudentDetailView.as_view(), name='detail'),
 ]
