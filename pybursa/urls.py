db04dedb7ae33a6948b55dad6be3fe0467c8150f
Verisana <1902Grand@gmail.com>
2018-02-22 13:21:49 +0500

    Rebuild on Django 1.10

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,7 @@
-from django.urls import include, path
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
