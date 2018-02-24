26f65c6771eacd353aaa9af2a1ee5a87dcd3a4e5
Verisana <1902Grand@gmail.com>
2018-02-25 02:41:55 +0500

    Done KZ5_Z2

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-admin.site.register(Course)
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    fields = ['subject', 'description', 'order']
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'short_description']
+    inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+def fullname(obj):
+    fullname.short_description = 'Full name'
+    return ("%s %s" % (obj.name, obj.surname))
+
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'surname']
+    list_display = [fullname, 'email', 'skype']
+    list_filter = ['courses']    
+    fieldsets = [('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
+                 ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+                 (None, {'fields': ['courses']})]
+    
+    #formfield_overrides = 
+
+admin.site.register(Student, StudentAdmin)
