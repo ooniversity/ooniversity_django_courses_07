b0e2b723560054c8f7ac499a3c14b412832429a6
Verisana <1902Grand@gmail.com>
2018-02-25 03:46:56 +0500

    Modified KZ5_Z2, changed filter to tuple

--- students/admin.py
+++ students/admin.py
@@ -6,7 +6,7 @@ class StudentAdmin(admin.ModelAdmin):
     search_fields = ['surname', 'email']
     list_display = ['full_name', 'email', 'skype']
     list_filter = ['courses']
-    filter_horizontal = ['courses']
+    filter_horizontal = ('courses',)
     fieldsets = [('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
                  ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
                  (None, {'fields': ['courses']})]
