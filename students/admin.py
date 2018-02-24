96ba04bfa3db25e3665eae39ae9cfe25764db30f
Verisana <1902Grand@gmail.com>
2018-02-25 03:31:07 +0500

    Modified KZ5_Z2, added horizontal filter

--- students/admin.py
+++ students/admin.py
@@ -5,16 +5,13 @@ from students.models import Student
 class StudentAdmin(admin.ModelAdmin):
     search_fields = ['surname', 'email']
     list_display = ['full_name', 'email', 'skype']
-    list_filter = ['courses']    
+    list_filter = ['courses']
+    filter_horizontal = ['courses']
     fieldsets = [('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
                  ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
                  (None, {'fields': ['courses']})]
-    
-    
+
     def full_name(self, obj):
         return ('%s %s' % (obj.name, obj.surname))
-
-    
-    #formfield_overrides = 
-
+        
 admin.site.register(Student, StudentAdmin)
