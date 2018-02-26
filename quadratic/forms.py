8534c664e28506c96cc0d4706e8f5fa32c5b746c
Verisana <1902Grand@gmail.com>
2018-02-27 03:53:08 +0500

    Added new validation rules

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -6,30 +6,30 @@ class QuadraticForm(forms.Form):
     c = forms.CharField(max_length=10, label='коэффициент c')
     
     def clean_a(self):
-        data = self.cleaned_data['a']
+        data = str(self.cleaned_data['a'])
         if data == '':
             raise forms.ValidationError("This field is required.")
-        elif data == 0:
+        elif data == '0':
             raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
-        elif isinstance(data, str):
-            if data not in '-0123456789':
+        elif len(data) >= 2:
+            if data[0] == data[1] == '-' or not data.lstrip('-').isdigit():
                 raise forms.ValidationError("коэффициент не целое число")         
         return data
     
     def clean_b(self):
-        data = self.cleaned_data['b']
+        data = str(self.cleaned_data['b'])
         if data == '':
             raise forms.ValidationError("This field is required.")
-        elif isinstance(data, str):
-            if data not in '-0123456789':
+        elif len(data) >= 2:
+            if data[0] == data[1] == '-' or not data.lstrip('-').isdigit():
                 raise forms.ValidationError("коэффициент не целое число")         
         return data
 
     def clean_c(self):
-        data = self.cleaned_data['c']
+        data = str(self.cleaned_data['c'])
         if data == '':
             raise forms.ValidationError("This field is required.")
-        elif isinstance(data, str):
-            if data not in '-0123456789':
-                raise forms.ValidationError("коэффициент не целое число")         
+        elif len(data) >= 2:
+            if data[0] == data[1] == '-' or not data.lstrip('-').isdigit():
+                raise forms.ValidationError("коэффициент не целое число")          
         return data
