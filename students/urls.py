ac5f75ffe65928e86cecacaa16ad40e78bc8cb7d
Verisana <1902Grand@gmail.com>
2018-02-27 17:51:13 +0500

    Done KZ7_Z3

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-
 from . import views
 
 app_name = 'students'
 urlpatterns = [
     url(r'^$', views.student_list, name='list_view'),
     url(r'^(?P<pk>[0-9]+)/$', views.student_detail, name='detail'),
-]
+    url(r'^add/$', views.create, name='add'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>[0-9]+)/$', views.remove, name='remove'),]
