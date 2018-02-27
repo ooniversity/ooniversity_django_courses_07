999421d269aba061879ed2aeffbda804596cce7b
Verisana <1902Grand@gmail.com>
2018-02-27 22:11:49 +0500

    Done KZ8_Z1

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'courses'
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.detailview, name='detail'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>[0-9]+)/$', views.remove, name='remove'),
+    url(r'^(?P<pk>[0-9]+)/add_lesson/$', views.add_lesson, name='add_lesson'),
 ]
