84238448aa0e57b563c066eba4d4406d85f2a34a
Verisana <1902Grand@gmail.com>
2018-02-28 15:40:55 +0500

    Half done KZ9_Z1

--- students/urls.py
+++ students/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'students'
 urlpatterns = [
-    url(r'^$', views.student_list, name='list_view'),
-    url(r'^(?P<pk>[0-9]+)/$', views.student_detail, name='detail'),
-    url(r'^add/$', views.create, name='add'),
-    url(r'^edit/(?P<pk>[0-9]+)/$', views.edit, name='edit'),
-    url(r'^remove/(?P<pk>[0-9]+)/$', views.remove, name='remove'),]
+    url(r'^$', views.StudentListView.as_view(), name='list_view'),
+    url(r'^(?P<pk>[0-9]+)/$', views.StudentDetailView.as_view(), name='detail'),
+    url(r'^add/$', views.StudentCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', views.StudentUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<pk>[0-9]+)/$', views.StudentDeleteView.as_view(), name='remove'),]
