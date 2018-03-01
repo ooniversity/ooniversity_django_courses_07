e1d88115a96fddae3b57ad5592d3f61d577a4b6d
Verisana <1902Grand@gmail.com>
2018-03-01 12:42:40 +0500

    Bugfix: modelForm and view path

--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from feedbacks.models import Feedback
 
-class CourseModelForm (forms.ModelForm):        
+class FeedbackForm(forms.ModelForm):        
     class Meta:
         model = Feedback
         fields = '__all__'
--- pybursa/views.py
+++ pybursa/views.py
@@ -1,24 +1,6 @@
-from django.shortcuts import render
 from django.views import generic
 from courses.models import Course
-from feedbacks.models import Feedback
-from django.core.mail import send_mail
-from django.urls import reverse_lazy
-from django.contrib.messages.views import SuccessMessageMixin
-from django.conf import settings
 
 class IndexView(generic.ListView):
     model = Course
     template_name = 'index.html'
-
-class FeedbackView(SuccessMessageMixin, generic.CreateView):
-    model = Feedback
-    fields = '__all__'
-    template_name = 'feedback.html'
-    success_url = reverse_lazy('feedback')
-    success_message = 'Thank you for your feedback! We will keep in touch with you very soon!'
-    
-    def form_valid(self, form):
-        responce = super().form_valid(form)
-        send_mail(form.cleaned_data['subject'], form.cleaned_data['message'], form.cleaned_data['from_email'], settings.ADMINS)
-        return responce
