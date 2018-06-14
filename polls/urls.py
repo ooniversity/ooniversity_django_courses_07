8c8b62327ca18696b45cc3fbe89961df1c8bae23
foxter@bigmir.net <foxter@bigmir.net>
2018-06-15 01:20:54 +0300

    fix task3.1

---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from django.contrib import admin
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
