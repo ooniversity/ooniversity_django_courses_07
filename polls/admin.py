d373d98d875516ff45c52f5233da5670ea3c112d
foxter@bigmir.net <foxter@bigmir.net>
2018-06-17 01:03:49 +0300

    task 3.2

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
-
-# Register your models here.
+admin.site.register(Choice)
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include, url
+from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('contact/', views.contact, name='contact'),
+    path('student_list/', views.student_list, name='student_list'),
+    path('student_detail/', views.student_detail, name='student_detail'),
 ]
