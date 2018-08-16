3e282517ed1fb1e7543837f142f3b282d9f6097e
Dmitry Odnorog <vibiy@yandex.ru>
2018-08-16 14:29:16 +0300

    week3 task1 corrections

--- students/models.py
+++ students/models.py
@@ -8,6 +8,6 @@ class Student(models.Model):
     date_of_birth = models.DateField()
     email = models.EmailField()
     phone = models.CharField(max_length=255)
-    adress = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     skype = models.CharField(max_length=255)
     courses = models.ManyToManyField(Course)
