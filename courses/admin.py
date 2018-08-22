029ce6215eeff0aa3a0a099298a0e4b76c9278a3
Dmitry Odnorog <vibiy@yandex.ru>
2018-08-22 11:04:08 +0300

    week 3 task 2

--- courses/admin.py
+++ courses/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Course, Lesson
 
 
-admin.site.register(Course)
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 0
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["name", "short_description"]
+    fieldsets = [
+                 (None, {'fields': ["name", "short_description", "description"]}),
+                ]
+    inlines = [LessonInline]
+    search_fields = ["name"]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
