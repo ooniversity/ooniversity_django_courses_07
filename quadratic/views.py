50a76b8b683fb9d0f9930913bad77cfe5aef98bd
Dmitry Odnorog <vibiy@yandex.ru>
2018-09-04 19:57:03 +0300

    corrections quadratic

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from quadratic.forms import QuadraticForm
+from math import sqrt
 
 
 def quadratic_results(request):
@@ -10,12 +11,12 @@ def quadratic_results(request):
             context['a'] = a = form.clean_a()
             context['b'] = b = form.cleaned_data['b']
             context['c'] = c = form.cleaned_data['c']
-            context['discriminant'] = D = b**2-4*a*c
+            context['discriminant'] = D = b*b - 4*a*c
             if D == 0:
-                context['x']  = -1*b/(2*a)
+                context['x']  = -b / (2 * a)
             elif D > 0:
-                context['x1'] = (-1*b + D**0.5)/(2*a)
-                context['x2'] = (-1*b - D**0.5)/(2*a)
+                context['x1'] = (-b + sqrt(D)) / (2 * a)
+                context['x2'] = (-b - sqrt(D)) / (2 * a)
     else:
         context = {'form': QuadraticForm()}
     return render(request, 'quadratic/results.html', context)
