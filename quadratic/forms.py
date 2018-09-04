7a41c60e6bdaca5a76ac254779ce1aff4925a564
Dmitry Odnorog <vibiy@yandex.ru>
2018-09-04 18:37:58 +0300

    update quadratic app and corrections

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -2,7 +2,14 @@ from django import forms
 
 
 class QuadraticForm(forms.Form):
-    a = forms.IntegerField()
-    b = forms.IntegerField()
-    c = forms.IntegerField()
+    a = forms.IntegerField(label='коэффициент a')
+    b = forms.IntegerField(label='коэффициент b')
+    c = forms.IntegerField(label='коэффициент c')
+    
+    def clean_a(self):
+        data = self.cleaned_data['a']
+        if data == 0:
+            raise forms.ValidationError('коэффициент при первом слагаемом не может быть равен нулю')
+        
+        return data
 
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,46 +1,21 @@
 from django.shortcuts import render
-from .forms import QuadraticForm
+from quadratic.forms import QuadraticForm
+
 
 def quadratic_results(request):
-    form = QuadraticForm()
-    a = request.GET.get('a', '')
-    b = request.GET.get('b', '')
-    c = request.GET.get('c', '')
-    context = {"a": a, "b": b, "c": c}
-    context['form'] = form
-    try:
-        a = int(a)
-        if a == 0:
-            context["a_add"] = "коэффициент при первом слагаемом не может быть равен нулю"
-    except:
-        if a == '':
-            context["a_add"] = "коэффициент не определен"
-        else:
-            context["a_add"] = "коэффициент не целое число"
-    try:
-        b = int(b)
-    except:
-        if b == '':
-            context["b_add"] = "коэффициент не определен"
-        else:
-            context["b_add"] = "коэффициент не целое число"
-    try:
-        c = int(c)
-    except:
-        if c == '':
-            context["c_add"] = "коэффициент не определен"
-        else:
-            context["c_add"] = "коэффициент не целое число"
-    if len(context) == 4:
-        D = int(b)**2-4*int(a)*int(c)
-        context["discriminant"] = "Дискриминант: " + str(D)
-        if D < 0:
-            context["discriminant_add"] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-        elif D == 0:
-            x = -1*int(b)/(2*a)
-            context["roots"] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = " + str(x)
-        else:
-            x1 = (-1*int(b) + D**0.5)/(2*a)
-            x2 = (-1*int(b) - D**0.5)/(2*a)
-            context["roots"] = "Квадратное уравнение имеет два действительных корня: x1 = " + str(x1) + " x2 = " + str(x2)
-    return render(request, "results.html", context)
+    form = QuadraticForm(request.GET)
+    context = {'form': form}
+    if form.is_valid():
+        data = form.cleaned_data
+        context = dict(list(context.items()) + list(data.items()))
+        a = context['a']
+        b = context['b']
+        c = context['c']
+        D = b**2-4*a*c
+        context['discriminant'] = D
+        if D == 0:
+            context['x']  = -1*b/(2*a)
+        elif D > 0:
+            context['x1'] = (-1*b + D**0.5)/(2*a)
+            context['x2'] = (-1*b - D**0.5)/(2*a)
+    return render(request, 'quadratic/results.html', context)
