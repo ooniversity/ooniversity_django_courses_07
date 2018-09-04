4ec6a6c9f3fdd550e25f0b25fbe9c44f91ce6568
Dmitry Odnorog <vibiy@yandex.ru>
2018-09-05 01:46:41 +0300

    7.3 new options in students app

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from . import views
+from students import views
 
 app_name = 'students'
 urlpatterns = [
     url(r'^$', views.list_view, name='list'),
     url(r'^(\d+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.create, name='create'),
+    url(r'^edit/(\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(\d+)/$', views.remove, name='remove'),
 ]
