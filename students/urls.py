273fbc3c100941f9330960fe804c072d840a30b8
truekorsar <paukne8001@yandex.ru>
2019-11-15 16:57:49 +0300

    Task done!

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('',home_page),
+    path('',course_list,name='student_list'),
+    path('<int:student_id>',student_detail,name='by_student'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from courses.models import Course
+from .models import Student
 
-# Create your views here.
+def course_list(request):
+    c_pk=request.GET.get('course_id',False)
+    if not c_pk:
+        students=Student.objects.all()
+    else:
+        course=Course.objects.get(pk=c_pk)
+        students=course.student_set.all()
+    context={'students':students}
+    return render(request,'students/students.html',context)
+def student_detail(request,student_id):
+    student=Student.objects.get(pk=student_id)
+    context={'student':student}
+    return render(request,'students/student_detail.html',context)
+        
