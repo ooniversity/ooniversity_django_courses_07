def50c6609648c798cd4560c6f5afca41caf0edc
mikola-s <suharev.nv@gmail.com>
2020-01-15 19:50:47 +0200

    some edit

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^contact/$', views.contact, name="contact"),
-    url(r'^student_list/$', views.student_list, name="student_list"),
-    url(r'^student_detail/$', views.student_detail, name="student_detail"),
+    url(r'^contact/', views.contact, name="contact"),
+    url(r'^student_list/', views.student_list, name="student_list"),
+    url(r'^student_detail/', views.student_detail, name="student_detail"),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
