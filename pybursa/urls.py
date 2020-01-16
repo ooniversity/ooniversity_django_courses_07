41fd4dc1976dea80a984c0d3c51007e4e7930e5c
mikola-s <suharev.nv@gmail.com>
2020-01-16 10:00:43 +0200

    control task 04 random edit

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
-from quadratic.views import quadratic_results
 
 urlpatterns = [
+    url(r'^quadratic/', include('quadratic.urls', namespace='quadratic')),
     url(r'^$', views.index, name="index"),
     url(r'^contact/$', views.contact, name="contact"),
     url(r'^student_list/$', views.student_list, name="student_list"),
     url(r'^student_detail/$', views.student_detail, name="student_detail"),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^quadratic/', include('quadratic.urls')),
 ]
