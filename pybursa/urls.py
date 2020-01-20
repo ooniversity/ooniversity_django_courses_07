b3397126676e91ecea1335b6e122cef1b306fbfe
mikola-s <suharev.nv@gmail.com>
2020-01-20 10:40:31 +0200

    pybursa urls.py add urls students courses

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^quadratic/results/', include('quadratic.urls')),
     url(r'^$', views.index, name="index"),
+    url(r'^courses/$', include('courses.urls', namespace='courses')),
+    url(r'^students/$', include('students.urls', namespace='students')),
+    url(r'^quadratic/results/', include('quadratic.urls')),
     url(r'^contact/$', views.contact, name="contact"),
     url(r'^student_list/$', views.student_list, name="student_list"),
     url(r'^student_detail/$', views.student_detail, name="student_detail"),
