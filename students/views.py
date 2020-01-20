a77c4b4e9c0a7cc21648e723c2892bd81dfbab25
mikola-s <suharev.nv@gmail.com>
2020-01-20 11:38:57 +0200

    students views.py add list_view

--- students/views.py
+++ students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Student
+from django.shortcuts import get_list_or_404
 
-# Create your views here.
+
+def list_view(request):
+    students = get_list_or_404(Student)
+    context = {'students': students}
+    return render(request, 'students/list.html', context=context)
