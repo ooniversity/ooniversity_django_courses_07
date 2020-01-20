8542fe1c6b5fb2a903e7b9bcaf7fc4a001dbefeb
mikola-s <suharev.nv@gmail.com>
2020-01-20 11:04:45 +0200

    courses views.py make detail

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Course, Lesson
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+def detail(request, course_pk):
+    course = get_object_or_404(Course, pk=course_pk)
+    lessons = get_object_or_404(Lesson, course_id=course_pk)
+    context = {'course': course, 'lessons': lessons}
+    return render(request, 'courses/detail.html', context=context)
