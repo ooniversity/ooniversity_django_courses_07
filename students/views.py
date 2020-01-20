172639cd0a104aaa8270eb19ba023f5f11504ba1
mikola-s <suharev.nv@gmail.com>
2020-01-20 11:42:31 +0200

    students views.py add detail

--- students/views.py
+++ students/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Student
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 
 def list_view(request):
     students = get_list_or_404(Student)
     context = {'students': students}
     return render(request, 'students/list.html', context=context)
+
+
+def detail(request, student_pk):
+    student = get_object_or_404(Student, id=student_pk)
+    context = {'student': student}
+    return render(request, 'students/detail.html', context=context)
