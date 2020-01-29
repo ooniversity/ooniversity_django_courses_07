818e3f4c866c01653c972838227bde004f6b93de
mikola-s <suharev.nv@gmail.com>
2020-01-29 10:07:36 +0200

    ct04 add start settings, url, template, views

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from . import views
+from pybursa import views
+from quadratic.views import quadratic_results
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^contact/', views.contact, name="contact"),
     url(r'^student_list/', views.student_list, name="student_list"),
     url(r'^student_detail/', views.student_detail, name="student_detail"),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include("polls.urls")),
     url(r'^admin/', admin.site.urls),
+    url(r'^quadratic/results', quadratic_results, name="results"),
 ]
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def quadratic_results(request):
+    return render(request, 'results.html')
