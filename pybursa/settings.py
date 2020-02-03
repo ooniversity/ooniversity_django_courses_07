5d2ee53168f867e12244bdfaad3e77b02479cbe8
mikola-s <suharev.nv@gmail.com>
2020-02-03 11:29:33 +0200

    ct04 some changes. 7 try

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'quadratic',
+    'quadratic.apps.QuadraticConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'pybursa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
