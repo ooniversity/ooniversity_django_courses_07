93c5cb083dcc0543c30f06f3c87a2722e3f53bff
mikola-s <suharev.nv@gmail.com>
2020-02-05 14:54:34 +0200

    ct 05_1 refill

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from pybursa import views
-from quadratic.views import quadratic_results
+from . import views
 
 urlpatterns = [
-    url(r'^quadratic/results/', quadratic_results, name="results"),
     url(r'^$', views.index, name="index"),
-    url(r'^contact/', views.contact, name="contact"),
-    url(r'^student_list/', views.student_list, name="student_list"),
-    url(r'^student_detail/', views.student_detail, name="student_detail"),
-    url(r'^polls/', include("polls.urls")),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
+    url(r'^students/', include('students.urls', namespace='students')),
+    url(r'^quadratic/results/', include('quadratic.urls')),
+    url(r'^contact/$', views.contact, name="contact"),
+    url(r'^student_list/$', views.student_list, name="student_list"),
+    url(r'^student_detail/$', views.student_detail, name="student_detail"),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Student
+from . import models
 
-admin.site.register(Student)
+admin.site.register(models.Student)
